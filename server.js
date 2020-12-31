af2114295e7cdfda847282a8a66a721370eec703
syk050 <syk050@naver.com>
2020-12-31 14:36:06 +0900

    게시글 수정 페이지

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get('/board', function(request, response){
 app.get('/board/:id', function(request, response){
   console.log('board id In : ' + request.params.id);
 
+  // 조회수 증가
   var rc = 0;
   client.query('SELECT readcount FROM board WHERE num = ?', [
     request.params.id
@@ -71,6 +72,7 @@ app.get('/board/:id', function(request, response){
     }
   });
 
+  // 게시글 표시
   fs.readFile('post.html', 'utf-8', function(error, data){
     client.query('SELECT * FROM board WHERE num = ?', [
       request.params.id
@@ -111,38 +113,28 @@ app.post('/creating_post', function(request, response){
   });
 });
 
-// 관리자 화면
-app.get('/admin', function (request, response) {
-  console.log('In admin');
+// 게시글 수정
+app.get('/edit/:id', function(request, response){
+  console.log('edit id In : ' + request.params.id);
 
-  fs.readFile('admin.html', 'utf8', function (error, data) {
-    var series;
-
-    client.query('SELECT DISTINCT series FROM products', function(error, result){
-      series=result;
-    });
-
-    client.query('SELECT * FROM products', function (error, results) {
+  fs.readFile('edit_post.html', 'utf8', function(err, data){
+    client.query('SELECT * FROM board WHERE num = ?', [
+      request.params.id
+    ], function(err, result){
       response.send(ejs.render(data, {
-        data: results,
-        category: series
+        data: result[0]
       }));
     });
   });
-}); 
-
-// 아이템 추가
-app.post('/admin', function (request, response) {
-  console.log('In Post admin');
-
-  var body = request.body;
-  console.log(body);
-  client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', [
-      body.name, body.model_number, body.series
-  ], function () {
-    response.redirect('/admin');
-  });
 });
+
+// 게시글 수정 완료
+app.post('/edit/:id', function(request, response){
+  console.log('update id: ' + request.params.id)
+
+
+});
+
 
 // 아이템 삭제
 app.get('/delete/:id', function (request, response) {
@@ -153,90 +145,36 @@ app.get('/delete/:id', function (request, response) {
  });
 });
 
-// 아이템 수정 페이지 화면
-app.get('/edit/:id', function (request, response) {
-  console.log('In GET edit :' + request.params.id);
-
-  fs.readFile('edit.html', 'utf8', function (error, data) {
-    client.query('SELECT * FROM products WHERE id = ?', [
-        request.params.id
-    ], function (error, result) {
-      response.send(ejs.render(data, {
-        data: result[0]
-      }));
-    });
-  });
-});
-
-// 아이템 수정
-app.post('/edit/:id', function (request, response) {
-  console.log('In POST edit :' + request.params.id);
-
-  var body = request.body;
-  console.log(body);
-  console.log(body.name);
-  client.query('UPDATE products SET name=?, modelnumber=?, series=? WHERE id=?', 
-      [body.name, body.model_number, body.series, request.params.id], function (error, result) {
-      if(error){
-        console.log(error);
-      }
-      response.send();
-  });
-});
-
-// 상품 관리 페이지
-app.get('/manage', function(request, response){
-  fs.readFile('manage.html', 'utf8', function (error, data) {
-    client.query('SELECT * FROM customer', function(error, result){
-      response.send(ejs.render(data, {
-        data: result
-      }));
-    });
-
-  });
-});
 
 // db에 초기 아이템 생성
 app.get('/__!create_item__', function(request, response) {
   console.log('In !create_item');
 
-  // 아이템이 없다면 생성
-  client.query('SELECT COUNT(*) AS cnt FROM products', function(err, result){
-    if (result[0].cnt == 0){
-      // csv 파일 읽어서 쿼리로 입력
-      fs.readFile('MOCK_DATA.csv', 'utf-8', function(error, data){
-          let dataArray = data.split(/\r?\n/);
-          dataArray.forEach(element => {
-            let test = element.split(',');
-            client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', [
-              test[0], test[1], test[2]
-            ]);
-          });
-      });
-      console.log('Create Item');
-    }
-  });
+  // // 아이템이 없다면 생성
+  // client.query('SELECT COUNT(*) AS cnt FROM products', function(err, result){
+  //   if (result[0].cnt == 0){
+  //     // csv 파일 읽어서 쿼리로 입력
+  //     fs.readFile('MOCK_DATA.csv', 'utf-8', function(error, data){
+  //         let dataArray = data.split(/\r?\n/);
+  //         dataArray.forEach(element => {
+  //           let test = element.split(',');
+  //           client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', [
+  //             test[0], test[1], test[2]
+  //           ]);
+  //         });
+  //     });
+  //     console.log('Create Item');
+  //   }
+  // });
 
   response.redirect('/admin');
 });
 
-// 아이템 개수 구하기
-app.get('/__!count_item__', function(request, response){
-  console.log('In !count_item');
-
-  client.query('SELECT COUNT(*) AS cnt FROM products', function(error, result){
-    if(!error){
-      console.log(result);
-      response.send('<p>' + result[0].cnt + '</p>')
-    }
-  });
-});
-
-// products 테이블 초기화
-app.get('/__!truncate_products__', function(request, response){
+// board 테이블 초기화
+app.get('/__!truncate_board__', function(request, response){
   console.log('In !ctruncate_products');
 
-  client.query('truncate products', function(error, result){
+  client.query('truncate board', function(error, result){
     if(!error){
       console.log(result);
     }
